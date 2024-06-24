604d8d8aa281a2f2210764b72f0edbaff82bfbaf
WalterCorsini <corsini.walter@gmail.com>
2024-06-24 17:23:43 +0200

    fix seeder

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -58,28 +58,3 @@ class ProjectsTableSeeder extends Seeder
 
     }
 }
-
-// namespace Database\Seeders;
-
-// use App\Models\Project;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-// use Illuminate\Database\Seeder;
-// use Faker\Generator as Faker;
-// use Illuminate\Support\Str;
-
-// class ProjectsTableSeeder extends Seeder
-// {
-//     /**
-//      * Run the database seeds.
-//      */
-//     public function run(Faker $faker): void
-//     {
-//         for ($i=0; $i < 10; $i++) {
-//             $newItem = new Project();
-//             $newItem->title = $faker->sentence(3);
-//             $newItem->description = $faker->text(255);
-//             $newItem->slug = Str::slug($newItem->title);
-//             $newItem->save();
-//         }
-//     }
-// }
